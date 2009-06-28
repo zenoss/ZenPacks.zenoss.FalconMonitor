b527a56945ff5a7fe617ed8370a8bcde4fc108e2
cholden <cholden@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-06-28 21:02:35 +0000

    Fix broken skins directory registration in Falcon and Brocade zenpacks reviewed by jp
    git-svn-id: http://dev.zenoss.com/svnint/trunk/enterprise/zenpacks/ZenPacks.zenoss.FalconMonitor@4962 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/FalconMonitor/__init__.py
+++ ZenPacks/zenoss/FalconMonitor/__init__.py
@@ -5,8 +5,13 @@
 ######################################################################
 
 import Globals
+import os
 from Products.CMFCore.DirectoryView import registerDirectory
-registerDirectory("skins", globals())
+
+skinsDir = os.path.join(os.path.dirname(__file__), 'skins')
+if os.path.isdir(skinsDir):
+    registerDirectory(skinsDir, globals())
+
 
 from Products.ZenModel.ZenPack import ZenPack as Base
 
